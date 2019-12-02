27d3bad36f5a69f2ede11df652fb3020dcd0aa96
WeihaoH <whuan028@uottawa.ca>
2019-12-02 13:27:47 -0500

    PU update

---
+++ target/generated-sources/annotations/persistence/UserTeamRelation_.java
@@ -0,0 +1,13 @@
+package persistence;
+
+import javax.annotation.Generated;
+import javax.persistence.metamodel.SingularAttribute;
+import javax.persistence.metamodel.StaticMetamodel;
+
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-12-02T13:27:00")
+@StaticMetamodel(UserTeamRelation.class)
+public class UserTeamRelation_ { 
+
+    public static volatile SingularAttribute<UserTeamRelation, Long> id;
+
+}
